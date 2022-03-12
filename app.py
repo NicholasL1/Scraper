4ed752b08f248d8790f6165387db469f1d215d5b
Nicholas Lachhman <nickla@my.yorku.ca>
2022-03-12 01:59:22 -0500

    created navbar

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
 from flask import Flask, render_template, url_for
+from bs4 import BeautifulSoup as soup
+import requests
+
+
+# with open("templates/index.html", "r") as f:
+#     doc = soup(f, "html.parser")
+#     word = doc.find("div", class_="tile-container")
+
+# print (word.string)
+
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route('/weather')
 def index():
-    return render_template('index.html')
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=308c52fb7d9fcbf8bb5484efe08e579e'
+    city = 'Brampton'
+
+    info = requests.get(url.format(city)).json()
+
+    holder = {
+        'city': city,
+        'temperature': info['main']['temp'],
+        'description': info['weather'][0]['description'],
+        'icon': info['weather'][0]['icon'],
+    }
+    print(holder)
+
+    return render_template('weather.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
